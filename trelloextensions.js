c348d00443347cdbf717d45ea69fd6b8fd701de3
frank hadder <luckyllama@gmail.com>
2012-06-15 14:49:22 -0500

    added hiding text in card title with [ ]

--- trelloextensions.js
+++ trelloextensions.js
@@ -1,123 +1,117 @@
 
 //internals
-var filtered = false, //watch for filtered cards
-    reg = /\[(.*)\]\s?/m; //parse regexp- accepts anything between []
-    //iconUrl = chrome.extension.getURL('images/storypoints-icon.png');
+var filtered = false; //watch for filtered cards
+var regEx = /\[(.*)\]\s?/m; //parse regexp- accepts anything between []
+var iconUrl = chrome.extension.getURL('images/clipboard.png');
 
 //what to do when DOM loads
 $(function(){
     //watch filtering
     $('.js-filter-toggle').live('mouseup',function(e){
-        setTimeout(function(){
+        setTimeout(function() {
             filtered=$('.js-filter-cards').hasClass('is-on');
         });
     });
 
-    $('body').bind('DOMSubtreeModified',function(e){
-        if($(e.target).hasClass('list'))
-            readList($(e.target));
+    $('body').bind('DOMSubtreeModified',function (e) {
+        if ($(e.target).is('.list')) {
+            initLists($(e.target));
+        }
     });
 
     $('.js-share').live('mouseup',function(){
         setTimeout(checkExport);
     });
 
-    function readList($c) {
-        $c.each(function(){
+    function initLists($lists) {
+        $lists.each(function () {
             if(!this.list) new List(this);
         });
     }
 
-    readList($('.list'));
+    initLists($('.list'));
 
+    $('<textarea>').attr('id', 'hidden-text-clipboard-helper').hide().appendTo($('body'));
 });
 
-//.list pseudo
-function List(el){
-    if(el.list)return;
-    el.list=this;
+var List = function (el) {
+    if (el.list) return;
+    el.list = this;
 
-    var $list=$(el),
-        busy = false,
-        to,
-        to2;
+    var $list = $(el);
+    var addCardTimeout;
 
-    var $total=$('<span class="list-total">')
-        .bind('DOMNodeRemovedFromDocument',function(){
-            clearTimeout(to);
-            to=setTimeout(function(){
-                $total.appendTo($list.find('.list-header h2'));
+    $list.on('DOMNodeInserted', function (e) {
+        var $el = $(e.target);
+        if ($el.is('.list-card') && !$el[0].card) {
+            clearTimeout(addCardTimeout);
+            addCardTimeout = setTimeout(initCards, 0, $el);
+        }
+    });
+
+    var initCards = function ($cards) {
+        $cards.each(function () {
+            if(!this.card) new Card(this);
+        });
+    };
+
+    initCards($('.list-card', $list));
+};
+
+var Card = function (el) {
+    if (el.card) return;
+    el.card = this;
+
+    var hidingText = false;
+    var hidingTextTimeout;
+    var $card = $(el)
+        .on('DOMNodeInserted', function (e) {
+            if (!hidingText && ($(e.target).is('.list-card-title') || e.target == $card[0])) {
+                clearTimeout(hidingTextTimeout);
+                hidingTextTimeout = setTimeout(hideText);
+            }
+        });
+
+    var addBadgeTimeout;
+    var badgeBeingAdded = false;
+    var $badge = $('<div>')
+        .addClass('badge hidden-text')
+        .css('background-image', 'url(' + iconUrl + ')')
+        .on('DOMSubtreeModified DOMNodeRemovedFromDocument', function (e) {
+            if (badgeBeingAdded || $badge.attr('title') === noHiddenText) {
+                return;
+            }
+            badgeBeingAdded = true;
+            clearTimeout(addBadgeTimeout);
+            addBadgeTimeout = setTimeout(function () {
+                $badge.prependTo($('.badges', $card));
+                badgeBeingAdded = false;
             });
         })
-        .appendTo($list.find('.list-header h2'));
-
-    $list.bind('DOMNodeInserted',function(e){
-        if($(e.target).hasClass('list-card') && !e.target.listCard) {
-            clearTimeout(to2);
-            to2=setTimeout(readCard,0,$(e.target));
-        }
-    });
-
-    function readCard($c){
-        $c.each(function(){
-            if($(this).hasClass('placeholder')) return;
-            if(!this.listCard) new ListCard(this);
+        .on('click', function (e) {
+            copyToClipboard($badge.attr('title'));
+            return false; //prevent click event from bubbling up
         });
-    }
 
-    readCard($list.find('.list-card'));
-}
-
-//.list-card pseudo
-function ListCard(el){
-    if(el.listCard)return;
-    el.listCard=this;
-
-    var points=-1,
-        parsed,
-        that=this,
-        busy=false,
-        busy2=false,
-        to,
-        to2,
-        ptitle,
-        $card=$(el)
-            .bind('DOMNodeInserted',function(e){
-                if(!busy && ($(e.target).hasClass('list-card-title') || e.target==$card[0])) {
-                    clearTimeout(to2);
-                    to2=setTimeout(getPoints);
-                }
-            }),
-        $badge=$('<div class="badge badge-points point-count" style="background-image: url('+iconUrl+')"/>')
-            .bind('DOMSubtreeModified DOMNodeRemovedFromDocument',function(e){
-                if(busy2)return;
-                busy2=true;
-                clearTimeout(to);
-                to = setTimeout(function(){
-                    $badge.prependTo($card.find('.badges'));
-                    busy2=false;
-                });
-            });
-
-    function getPoints(){
-        var $title=$card.find('a.list-card-title');
-        if(!$title[0]||busy)return;
-        busy=true;
-        var title=$title[0].text;
-        parsed=title.match(reg);
-        points=parsed?parsed[1]:-1;
-        if($card.parent()[0]){
-            $title[0].textContent = title.replace(reg,'');
-            $badge.text(that.points);
-            $badge.attr({title: 'This card has '+that.points+' storypoint' + (that.points == 1 ? '.' : 's.')});
+    var noHiddenText = '#{none}#';
+    var hideText = function () {
+        var $title = $('a.list-card-title', $card);
+        if ($title[0]) {
+            hidingText = true;
+            var titleText = $title[0].text;
+            var matches = titleText.match(regEx);
+            var hiddenText = matches ? matches[1] : noHiddenText;
+            if ($card.parent()[0]) {
+                $title[0].textContent = titleText.replace(regEx, '');
+                $badge.attr({title: hiddenText });
+            }
         }
-        busy=false;
-    }
+        hidingText = false;
+    };
 
-    this.__defineGetter__('points',function(){
-        //don't add to total when filtered out
-        return parsed&&(!filtered||($card.css('opacity')==1 && $card.css('display')!='none'))?points:'';
-    });
+    hideText();
+};
 
-    getPoints();
-}
+var copyToClipboard = function(text) {
+    chrome.extension.sendRequest({ text: text });
+};
